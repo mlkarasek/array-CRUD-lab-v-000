9db553de0800dff19159ba4aed86e3d34685d7ec
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 22:21:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["red", "blue", "organge", "yellow"]
+  ["red", "blue", "orange", "yellow"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  create_an_array << 
 end
 
 def add_element_to_start_of_array(array, element)
